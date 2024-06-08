1f80d834627469e8745b1342e95f2573047aa0bb
Muhammad Abdullah <muhabdullah12101@gmail.com>
2024-06-08 14:09:03 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const headerVideo = document.querySelector('.header-video');
-const header = document.querySelector('header');
-const main = document.querySelector('main');
-const loading = document.querySelector('#loading');
 
 document.addEventListener('DOMContentLoaded', () => {
+    const headerVideo = document.querySelector('.header-video');
+    const header = document.querySelector('header');
+    const main = document.querySelector('main');
+    const loading = document.querySelector('#loading');
+
     headerVideo.addEventListener('canplaythrough', () => {
         loading.classList.add('hidden');
         main.classList.remove('hidden');
